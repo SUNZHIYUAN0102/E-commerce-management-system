4ab83a2f39efacc89a6dd2d36865c66925d4577f
致远 孙 <494726814@qq.com>
2021-10-06 01:18:47 +0700

    before custom validate

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -9,14 +9,23 @@
     <el-card class="box-card">
       <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容">
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
             <el-button
+              @click="getUserList"
               slot="append"
               icon="el-icon-search"
-            ></el-button> </el-input
-        ></el-col>
+            ></el-button>
+          </el-input>
+        </el-col>
         <el-col :span="8"
-          ><el-button type="primary">添加用户</el-button></el-col
+          ><el-button type="primary" @click="dialogVisible = true"
+            >添加用户</el-button
+          ></el-col
         >
       </el-row>
 
@@ -93,6 +102,38 @@
       >
       </el-pagination>
     </el-card>
+
+    <el-dialog title="添加用户" :visible.sync="dialogVisible" width="50%">
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="addForm.password"></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -107,6 +148,44 @@ export default {
       },
       userList: [],
       total: 0,
+      dialogVisible: false,
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile:""
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"],
+          },
+        ],
+        mobile: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+        ],
+      },
     };
   },
   created() {
