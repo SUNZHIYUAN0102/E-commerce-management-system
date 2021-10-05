0638e6e29a219b0e5e98d2d520b496f23bb653b0
致远 孙 <494726814@qq.com>
2021-10-05 17:01:54 +0700

    template of home page finish

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,121 @@
 <template>
-    <div>
-        <el-button @click="logout" type="info">退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>E-commerce Management System</span>
+      </div>
+      <el-button @click="logout" type="info">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse? '64px': '200px'">
+        <div @click="toggleCollapse" class="toggle-button">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          :unique-opened="true"
+          :collapse=isCollapse
+          :collapse-transition="false"
+        >
+
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+
+            <el-menu-item :index="child.id + ''" v-for="child in item.children" :key="child.id">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{child.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  methods: {
-    logout () {
-      window.sessionStorage.clear('token')
-      this.$router.push('/login')
+  data(){
+    return{
+      isCollapse: false,
+      menuList:[],
+      iconsObj:{
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      }
     }
-  }
-}
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear("token");
+      this.$router.push("/login");
+    },
+    async getMenuList() {
+      const {data:res} = await this.$http.get("menus")
+      console.log(res);
+      if(res.meta.status!=200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data
+    },
+    toggleCollapse(){
+      this.isCollapse = !this.isCollapse
+    }
+  },
+  created() {
+    this.getMenuList();
+  },
+};
 </script>
+
+<style scoped>
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0;
+  color: #fff;
+  font-size: 20px;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+.el-header div span {
+  margin-left: 15px;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-aside .el-menu{
+  border-right: none;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.home-container {
+  height: 100%;
+}
+.iconfont{
+  margin-right: 10px;
+}
+.toggle-button{
+  background-color: #4a5064;
+  color: #fff;
+  font-size: 10px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
