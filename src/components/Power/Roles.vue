eeecce177d78b1b577d7b4460244a13126db6ba5
致远 孙 <494726814@qq.com>
2021-10-06 19:27:53 +0700

    finish tree in table of roles

--- src/components/Power/Roles.vue
+++ src/components/Power/Roles.vue
@@ -15,7 +15,43 @@
         </el-col>
       </el-row>
       <el-table :data="roleList" stripe style="width: 100%" border>
-        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="expand">
+          <template v-slot="scope">
+            <el-row
+              v-for="(item1, it1) in scope.row.children"
+              :key="item1.id"
+              :class="['bdbottom', 'vcenter', it1 === 0 ? 'bdtop' : '']"
+            >
+              <el-col :span="5">
+                <el-tag>{{ item1.authName }}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <el-col :span="19">
+                <el-row
+                  v-for="(item2, it2) in item1.children"
+                  :key="item2.id"
+                  :class="['vcenter', it2 === 0 ? '' : 'bdtop']"
+                >
+                  <el-col :span="6">
+                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag
+                      type="warning"
+                      v-for="item3 in item2.children"
+                      :key="item3.id"
+                      closable
+                      @close="removeRightById"
+                    >
+                      {{ item3.authName }}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column prop="roleName" label="角色名称"> </el-table-column>
         <el-table-column prop="roleDesc" label="角色描述"> </el-table-column>
@@ -224,9 +260,40 @@ export default {
         this.$message.info("已取消删除");
       }
     },
+    async removeRightById() {
+      const confirmResult = await this.$confirm(
+        "此操作将永久删除该文件, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).catch((err) => err);
+
+      if(confirmResult != 'confirm') return this.$message.info ="取消删除";
+
+      console.log('meow');
+    },
   },
   created() {
     this.getRoleList();
   },
 };
 </script>
+
+<style scoped>
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.el-tag {
+  margin: 7px;
+}
+.vcenter {
+  display: flex;
+  align-items: center;
+}
+</style>
