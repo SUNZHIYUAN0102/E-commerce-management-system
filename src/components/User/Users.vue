848e5cc2bd9b632e5bb274e0d9dd543b32d83303
致远 孙 <494726814@qq.com>
2021-10-07 17:23:24 +0700

    finish set roles to user

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -84,6 +84,7 @@
               :enterable="false"
             >
               <el-button
+                @click="showSetRoleDialog(scope.row)"
                 type="warning"
                 icon="el-icon-setting"
                 size="mini"
@@ -170,6 +171,36 @@
         <el-button type="primary" @click="editUser">确 定</el-button>
       </span>
     </el-dialog>
+
+    <el-dialog
+      title="分配角色"
+      :visible.sync="setRoleDialogVisible"
+      width="50%"
+      @close = "clearRoleInfo"
+    >
+      <div>
+        <p>当前用户： {{ userInfo.username }}</p>
+        <p>当前角色： {{ userInfo.role_name }}</p>
+        <p>
+          分配新角色：
+          <el-select v-model="selectedRoleId" placeholder="请选择">
+            <el-option
+              v-for="item in rolesList"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            >
+            </el-option>
+          </el-select>
+        </p>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveRoleInfo"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -247,6 +278,11 @@ export default {
           { validator: checkMobile, trigger: "blur" },
         ],
       },
+
+      setRoleDialogVisible: false,
+      userInfo: {},
+      rolesList: [],
+      selectedRoleId:''
     };
   },
   created() {
@@ -353,6 +389,32 @@ export default {
         this.$message.info("已取消删除");
       }
     },
+
+    async showSetRoleDialog(userInfo) {
+      this.userInfo = userInfo;
+      this.setRoleDialogVisible = true;
+      const { data: res } = await this.$http.get("roles");
+      if (res.meta.status != 200)
+        return this.$message.error("获取角色列表失败");
+
+      this.rolesList = res.data;
+    },
+
+    async saveRoleInfo(){
+      if(!this.selectedRoleId){
+        return this.$message.error('请选择要分配的角色');
+      }
+
+      const{data:res} = await this.$http.put(`users/${this.userInfo.id}/role`,{rid:this.selectedRoleId});
+      if(res.meta.status!=200) return this.$message.error('分配角色失败');
+      this.$message.success('分配角色成功');
+      this.getUserList();
+      this.setRoleDialogVisible = false;
+    },
+    clearRoleInfo(){
+      this.selectedRoleId = '';
+      this.setRoleDialogVisible = false;
+    }
   },
 };
 </script>
